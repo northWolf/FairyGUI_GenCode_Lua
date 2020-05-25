9701290bbce8023fec82bece9a43492eb8d269c7
lz3263540140 <3263540140@qq.com>
2020-05-25 19:40:31 +0800

    完善Lua导出功能 和 自定义属性。

--- FairyGUI_GenCode_Lua/main.lua
+++ FairyGUI_GenCode_Lua/main.lua
@@ -1,13 +1,111 @@
+--region LuaCodeWriter
+local LuaCodeWriter = fclass()
+
+function LuaCodeWriter:ctor(config)
+    config = config or {}
+    self.blockStart = config.blockStart or '{'
+    self.blockEnd = config.blockEnd or '}'
+    self.blockFromNewLine = config.blockFromNewLine
+    if self.blockFromNewLine == nil then
+        self.blockFromNewLine = true
+    end
+    if config.usingTabs then
+        self.indentStr = '\t'
+    else
+        self.indentStr = '    '
+    end
+    self.usingTabs = config.usingTabs
+    self.endOfLine = config.endOfLine or '\n'
+    self.lines = {}
+    self.indent = 0
+
+    self:writeMark()
+end
+
+function LuaCodeWriter:writeMark()
+    table.insert(self.lines, '--- This is an automatically generated class by FairyGUI. Please do not modify it. ---')
+    table.insert(self.lines, '')
+end
+
+function LuaCodeWriter:writeln(format, ...)
+    if not format then
+        table.insert(self.lines, '')
+        return
+    end
+
+    local str = ''
+    for i = 0, self.indent - 1 do
+        str = str .. self.indentStr
+    end
+    str = str .. string.format(format, ...)
+    table.insert(self.lines, str)
+
+    return self
+end
+
+function LuaCodeWriter:startBlock()
+    if self.blockFromNewLine or #self.lines == 0 then
+        self:writeln(self.blockStart)
+    else
+        local str = self.lines[#self.lines]
+        self.lines[#self.lines] = str .. ' ' .. self.blockStart
+    end
+    self.indent = self.indent + 1
+
+    return self
+end
+
+function LuaCodeWriter:endBlock()
+    self.indent = self.indent - 1
+    self:writeln(self.blockEnd)
+
+    return self
+end
+
+function LuaCodeWriter:incIndent()
+    self.indent = self.indent + 1
+
+    return self
+end
+
+function LuaCodeWriter:decIndent()
+    self.indent = self.indent - 1
+
+    return self
+end
+
+function LuaCodeWriter:reset()
+    if #self.lines > 0 then
+        self.lines = {}
+    end
+    self.indent = 0
+
+    self:writeMark()
+end
+
+function LuaCodeWriter:tostring()
+    return table.concat(self.lines, self.endOfLine)
+end
+
+function LuaCodeWriter:save(filePath)
+    local str = table.concat(self.lines, self.endOfLine)
+
+    CS.System.IO.File.WriteAllText(filePath, str)
+end
+
+--endregion
+
 local projectCustomPropertiesDic;
 
 local customPropKeys = {
-    key_gen_lua = {name = "key_gen_lua",default_value = "true"},
-    key_lua_file_extension_name = {name = "key_lua_file_extension_name",default_value = "lua"},
-    key_lua_class_prefix = {name = "key_lua_class_prefix",default_value = ""},
+    key_gen_lua = { name = "key_gen_lua", default_value = "true" },
+    key_lua_file_extension_name = { name = "key_lua_file_extension_name", default_value = "lua" },
+    key_lua_path_root = { name = "key_lua_path_root", default_value = "UIGenCode/" },
+    key_wrapper_namespace = { name = "key_wrapper_namespace", default_value = "CS.FairyGUI" },
 }
 
 local function get_project_custom_property_value(key_name)
-    if(projectCustomPropertiesDic and projectCustomPropertiesDic:ContainsKey(key_name)) then
+    if (projectCustomPropertiesDic and projectCustomPropertiesDic:ContainsKey(key_name)) then
         return projectCustomPropertiesDic:get_Item(key_name);
     else
         return customPropKeys[key_name].default_value;
@@ -15,236 +113,160 @@ local function get_project_custom_property_value(key_name)
 end
 
 function onPublish(handler)
-    if not handler.genCode then return end
+    if not handler.genCode then
+        return
+    end
 
     projectCustomPropertiesDic = App.project:GetSettings("CustomProperties").elements;
 
     local gen_lua = get_project_custom_property_value(customPropKeys.key_gen_lua.name);
-    if(gen_lua == "true") then
+    if (gen_lua == "true") then
         handler.genCode = false --prevent default output
         App.consoleView:Clear();
         fprint("Handling gen lua code in plugin.")
-        genCode2lua(handler)
+        genCode(handler)
     end
 end
 
--- this is copied from Editor-Install-Path/Resources/Data/StreamingAssets/Scripts
 function genCode(handler)
     local settings = handler.project:GetSettings("Publish").codeGeneration
     local codePkgName = handler:ToFilename(handler.pkg.name); --convert chinese to pinyin, remove special chars etc.
-    local exportCodePath = handler.exportCodePath..'/'..codePkgName
-    local namespaceName = codePkgName
-    local ns = 'fgui'
-
-    if settings.packageName~=nil and settings.packageName~='' then
-        namespaceName = settings.packageName..'.'..namespaceName;
-    end
-
-    --CollectClasses(stripeMemeber, stripeClass, fguiNamespace)
-    local classes = handler:CollectClasses(settings.ignoreNoname, settings.ignoreNoname, ns)
-    handler:SetupCodeFolder(exportCodePath, "ts") --check if target folder exists, and delete old files
-
-    local getMemberByName = settings.getMemberByName
-
-    local classCnt = classes.Count
-    local writer = CodeWriter.new({ blockFromNewLine=false, usingTabs = true  })
-    for i=0,classCnt-1 do
-        local classInfo = classes[i]
-        local members = classInfo.members
-        local references = classInfo.references
-        writer:reset()
-
-        local refCount = references.Count
-        if refCount>0 then
-            for j=0,refCount-1 do
-                local ref = references[j]
-                writer:writeln('import %s from "./%s";', ref, ref)
-            end
-            writer:writeln()
-        end
-
-        writer:writeln('export default class %s extends %s', classInfo.className, classInfo.superClassName)
-        writer:startBlock()
-
-        local memberCnt = members.Count
-        for j=0,memberCnt-1 do
-            local memberInfo = members[j]
-            writer:writeln('public %s:%s;', memberInfo.varName, memberInfo.type)
-        end
-        writer:writeln('public static URL:string = "ui://%s%s";', handler.pkg.id, classInfo.classId)
-        writer:writeln()
-
-        writer:writeln('public static createInstance():%s', classInfo.className)
-        writer:startBlock()
-        writer:writeln('return <%s><any>%s.UIPackage.createObject("%s", "%s");', classInfo.className, ns, handler.pkg.name, classInfo.resName)
-        writer:endBlock()
-        writer:writeln()
-
-        writer:writeln('protected onConstruct():void')
-        writer:startBlock()
-        for j=0,memberCnt-1 do
-            local memberInfo = members[j]
-            if memberInfo.group==0 then
-                if getMemberByName then
-                    writer:writeln('this.%s = <%s><any>this.getChild("%s");', memberInfo.varName, memberInfo.type, memberInfo.name)
-                else
-                    writer:writeln('this.%s = <%s><any>this.getChildAt(%s);', memberInfo.varName, memberInfo.type, memberInfo.index)
-                end
-            elseif memberInfo.group==1 then
-                if getMemberByName then
-                    writer:writeln('this.%s = this.getController("%s");', memberInfo.varName, memberInfo.name)
-                else
-                    writer:writeln('this.%s = this.getControllerAt(%s);', memberInfo.varName, memberInfo.index)
-                end
-            else
-                if getMemberByName then
-                    writer:writeln('this.%s = this.getTransition("%s");', memberInfo.varName, memberInfo.name)
-                else
-                    writer:writeln('this.%s = this.getTransitionAt(%s);', memberInfo.varName, memberInfo.index)
-                end
-            end
-        end
-        writer:endBlock()
-
-        writer:endBlock() --class
-
-        writer:save(exportCodePath..'/'..classInfo.className..'.ts')
-    end
-
-    writer:reset()
-
-    local binderName = codePkgName..'Binder'
-
-    for i=0,classCnt-1 do
-        local classInfo = classes[i]
-        writer:writeln('import %s from "./%s";', classInfo.className, classInfo.className)
-    end
-
-    writer:writeln()
-    writer:writeln('export default class %s', binderName)
-    writer:startBlock()
-
-    writer:writeln('public static bindAll():void')
-    writer:startBlock()
-    for i=0,classCnt-1 do
-        local classInfo = classes[i]
-        writer:writeln('%s.UIObjectFactory.setExtension(%s.URL, %s);', ns, classInfo.className, classInfo.className)
-    end
-    writer:endBlock() --bindall
-
-    writer:endBlock() --class
-
-    writer:save(exportCodePath..'/'..binderName..'.ts')
-end
-
-function genCode2lua(handler)
-    local settings = handler.project:GetSettings("Publish").codeGeneration
-    local codePkgName = handler:ToFilename(handler.pkg.name); --convert chinese to pinyin, remove special chars etc.
-    local exportCodePath = handler.exportCodePath..'/'..codePkgName
-    local lua_class_prefix = get_project_custom_property_value(customPropKeys.key_lua_class_prefix.name);
+    fprint("codePkgName = " .. codePkgName);
+    local exportCodePath = handler.exportCodePath .. '/' .. codePkgName
+    fprint("exportCodePath = " .. exportCodePath);
     local lua_file_extension_name = get_project_custom_property_value(customPropKeys.key_lua_file_extension_name.name);
 
     local namespaceName = codePkgName
-    local ns = 'fgui'
+    local key_wrapper_namespace = get_project_custom_property_value(customPropKeys.key_wrapper_namespace.name);
 
-    if settings.packageName~=nil and settings.packageName~='' then
-        namespaceName = settings.packageName..'.'..namespaceName;
+    if settings.packageName ~= nil and settings.packageName ~= '' then
+        namespaceName = settings.packageName .. '.' .. namespaceName;
     end
 
     --CollectClasses(stripeMemeber, stripeClass, fguiNamespace)
-    local classes = handler:CollectClasses(settings.ignoreNoname, settings.ignoreNoname, ns)
+    local classes = handler:CollectClasses(settings.ignoreNoname, settings.ignoreNoname, key_wrapper_namespace)
     handler:SetupCodeFolder(exportCodePath, lua_file_extension_name) --check if target folder exists, and delete old files
 
     local getMemberByName = settings.getMemberByName
+    local classTemplateTxt = CS.System.IO.File.ReadAllText(PluginPath .. "/class_template.txt");
 
     local classCnt = classes.Count
-    local writer = CodeWriter.new({ blockFromNewLine=false, usingTabs = true  })
-    for i=0,classCnt-1 do
+    local writer = LuaCodeWriter.new({ blockFromNewLine = false, usingTabs = true })
+    for i = 0, classCnt - 1 do
+        local _classTemplateTxt = classTemplateTxt;
         local classInfo = classes[i]
         local members = classInfo.members
         local references = classInfo.references
         writer:reset()
 
         local refCount = references.Count
-        if refCount>0 then
-            for j=0,refCount-1 do
+        if refCount > 0 then
+            for j = 0, refCount - 1 do
                 local ref = references[j]
-                writer:writeln('import %s from "./%s";', ref, ref)
+                writer:writeln('local %s = require("%s/%s");', ref, exportCodePath, ref)
             end
             writer:writeln()
         end
 
-        writer:writeln('export default class %s extends %s', classInfo.className, classInfo.superClassName)
-        writer:startBlock()
-
-        local memberCnt = members.Count
-        for j=0,memberCnt-1 do
-            local memberInfo = members[j]
-            writer:writeln('public %s:%s;', memberInfo.varName, memberInfo.type)
+        _classTemplateTxt = string.gsub(_classTemplateTxt, "$className", classInfo.className);
+        _classTemplateTxt = string.gsub(_classTemplateTxt, "$superClassName", classInfo.superClassName);
+        if (key_wrapper_namespace ~= "") then
+            _classTemplateTxt = string.gsub(_classTemplateTxt, "$namespace", key_wrapper_namespace .. ".");
+        else
+            _classTemplateTxt = string.gsub(_classTemplateTxt, "$namespace", "");
         end
-        writer:writeln('public static URL:string = "ui://%s%s";', handler.pkg.id, classInfo.classId)
-        writer:writeln()
-
-        writer:writeln('public static createInstance():%s', classInfo.className)
-        writer:startBlock()
-        writer:writeln('return <%s><any>%s.UIPackage.createObject("%s", "%s");', classInfo.className, ns, handler.pkg.name, classInfo.resName)
-        writer:endBlock()
-        writer:writeln()
-
-        writer:writeln('protected onConstruct():void')
-        writer:startBlock()
-        for j=0,memberCnt-1 do
+        local _classMemberAnnotation = "";
+        local memberCnt = members.Count
+        for j = 0, memberCnt - 1 do
+            if (j > 0) then
+                _classMemberAnnotation = _classMemberAnnotation .. "\n";
+            end
             local memberInfo = members[j]
-            if memberInfo.group==0 then
+            _classMemberAnnotation = _classMemberAnnotation .. string.format('---@field public %s %s', memberInfo.varName, memberInfo.type);
+        end
+        _classTemplateTxt = string.gsub(_classTemplateTxt, "$classMemberAnnotation", _classMemberAnnotation);
+
+        local _urlValue = string.format('"ui://%s%s"', handler.pkg.id, classInfo.classId)
+        _classTemplateTxt = string.gsub(_classTemplateTxt, "$urlValue", _urlValue);
+
+        _classTemplateTxt = string.gsub(_classTemplateTxt, "$uiPackageName", handler.pkg.name);
+        _classTemplateTxt = string.gsub(_classTemplateTxt, "$uiResName", classInfo.resName);
+
+        local _classMemberInstatiation = "";
+        for j = 0, memberCnt - 1 do
+            local memberInfo = members[j]
+            if j > 0 then
+                _classMemberInstatiation = _classMemberInstatiation .. "\n";
+            end
+            _classMemberInstatiation = _classMemberInstatiation .. "\t";
+            if memberInfo.group == 0 then
                 if getMemberByName then
-                    writer:writeln('this.%s = <%s><any>this.getChild("%s");', memberInfo.varName, memberInfo.type, memberInfo.name)
+                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetChild("%s");', memberInfo.varName, memberInfo.name);
                 else
-                    writer:writeln('this.%s = <%s><any>this.getChildAt(%s);', memberInfo.varName, memberInfo.type, memberInfo.index)
+                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetChildAt("%s");', memberInfo.varName, memberInfo.index);
                 end
-            elseif memberInfo.group==1 then
+            elseif memberInfo.group == 1 then
                 if getMemberByName then
-                    writer:writeln('this.%s = this.getController("%s");', memberInfo.varName, memberInfo.name)
+                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetController("%s");', memberInfo.varName, memberInfo.name);
                 else
-                    writer:writeln('this.%s = this.getControllerAt(%s);', memberInfo.varName, memberInfo.index)
+                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetControllerAt("%s");', memberInfo.varName, memberInfo.index);
                 end
             else
                 if getMemberByName then
-                    writer:writeln('this.%s = this.getTransition("%s");', memberInfo.varName, memberInfo.name)
+                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetTransition("%s");', memberInfo.varName, memberInfo.name);
                 else
-                    writer:writeln('this.%s = this.getTransitionAt(%s);', memberInfo.varName, memberInfo.index)
+                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetTransitionAt("%s");', memberInfo.varName, memberInfo.index);
                 end
             end
         end
-        writer:endBlock()
+        _classTemplateTxt = string.gsub(_classTemplateTxt, "$classMemberInstantiation", _classMemberInstatiation);
+        writer:writeln('%s', _classTemplateTxt);
 
-        writer:endBlock() --class
-
-        writer:save(exportCodePath..'/'..classInfo.className..'.'..lua_file_extension_name)
+        writer:save(exportCodePath .. '/' .. classInfo.className .. '.' .. lua_file_extension_name)
     end
 
     writer:reset()
 
-    local binderName = codePkgName..'Binder'
+    local binderTemplateTxt = CS.System.IO.File.ReadAllText(PluginPath .. "/binder_template.txt");
 
-    for i=0,classCnt-1 do
+    local binderName = codePkgName .. 'Binder'
+
+    local _requireStatement = "";
+    local lua_path_root = get_project_custom_property_value(customPropKeys.key_lua_path_root.name);
+    lua_path_root = string.gsub(lua_path_root, "/", ".");
+    lua_path_root = string.gsub(lua_path_root, "\\", ".");
+    for i = 0, classCnt - 1 do
+        if (i > 0) then
+            _requireStatement = _requireStatement .. "\n";
+        end
         local classInfo = classes[i]
-        writer:writeln('import %s from "./%s";', classInfo.className, classInfo.className)
+        _requireStatement = _requireStatement .. string.format('local %s = require("%s%s.%s");', classInfo.className, lua_path_root,codePkgName, classInfo.className);
     end
 
-    writer:writeln()
-    writer:writeln('export default class %s', binderName)
-    writer:startBlock()
+    binderTemplateTxt = string.gsub(binderTemplateTxt, "$requireStatement", _requireStatement);
+    binderTemplateTxt = string.gsub(binderTemplateTxt, "$binderClassName", binderName);
 
-    writer:writeln('public static bindAll():void')
-    writer:startBlock()
-    for i=0,classCnt-1 do
+    local _bindStatement = "";
+    for i = 0, classCnt - 1 do
+        if (i > 0) then
+            _bindStatement = _bindStatement .. "\n";
+        end
         local classInfo = classes[i]
-        writer:writeln('%s.UIObjectFactory.setExtension(%s.URL, %s);', ns, classInfo.className, classInfo.className)
+        _bindStatement = _bindStatement .. "\t";
+
+        if (key_wrapper_namespace ~= "") then
+            _bindStatement = _bindStatement .. string.format('%sUIObjectFactory.SetExtension(%s.URL, %s);', key_wrapper_namespace .. ".", classInfo.className, classInfo.className);
+        else
+            _bindStatement = _bindStatement .. string.format('UIObjectFactory.SetExtension(%s.URL, %s);', classInfo.className, classInfo.className);
+        end
+
+
     end
-    writer:endBlock() --bindall
+    binderTemplateTxt = string.gsub(binderTemplateTxt, "$bindStatement", _bindStatement);
+    writer:writeln('%s', binderTemplateTxt);
 
-    writer:endBlock() --class
-
-    writer:save(exportCodePath..'/'..binderName..'.'..lua_file_extension_name)
+    writer:save(exportCodePath .. '/' .. binderName .. '.' .. lua_file_extension_name)
 
 end
+
+
--- README.md
+++ README.md
@@ -1,37 +1,29 @@
 # FairyGUI_GenCode_Lua
 
 #### 介绍
-FairyGUI.v2020 编辑器插件，用于生成Lua视图代码绑定。
-
-#### 软件架构
-软件架构说明
-
+FairyGUI.v2020 编辑器插件，用于生成 Lua 视图绑定代码。
 
 #### 安装教程
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+克隆本项目到 FairyGUI 工程的 plugins 目录中，刷新 FairyGUI 编辑器插件窗口或重启编辑器。
 
 #### 使用说明
 
-1.  xxxx
-2.  xxxx
-3.  xxxx
+1.  本项目理论上适用使用 Lua 作为视图逻辑的游戏引擎，但只在 Unity 引擎下进行了测试。
+2.  使用本插件前，需要先熟悉编辑器发布代码的功能，并开启发布代码。
+3.  使用__项目设置-自定义属性__为插件功能赋值。
+```
+local customPropKeys = {
+    key_gen_lua = { name = "key_gen_lua", default_value = "true" },
+    key_lua_file_extension_name = { name = "key_lua_file_extension_name", default_value = "lua" },
+    key_lua_path_root = { name = "key_lua_path_root", default_value = "UIGenCode/" },
+    key_wrapper_namespace = { name = "key_wrapper_namespace", default_value = "" },
+}
+```
+* key_gen_lua 是否生成Lua代码，默认为值 true
+* key_lua_file_extension_name Lua文件的扩展名，默认值为 lua
+* key_lua_path_root 生成文件相对于Lua执行根路径的相对路径，默认值为 UIGenCode/
+* key_wrapper_namespace 引擎层 FairyGUI 导出代码的命名空间，默认值为 CS.FairyGUI
 
-#### 参与贡献
-
-1.  Fork 本仓库
-2.  新建 Feat_xxx 分支
-3.  提交代码
-4.  新建 Pull Request
-
-
-#### 码云特技
-
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  码云官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解码云上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是码云最有价值开源项目，是码云综合评定出的优秀开源项目
-5.  码云官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  码云封面人物是一档用来展示码云会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+4.  生成文件是根据模板文件 __class_template.txt__ 和 __binder_template.txt__ 生成,以 __$XXX__ 作为占位符,可以在此基础上继续自定义扩展。
+5.  本项目参考了官方的 [GenCode](https://github.com/fairygui/FairyGUI-Editor) 插件，作为补充，提供Lua生成作为一个选项。
