c153415adf39b8d3fae744c24c786b1e456bf2dc
lz3263540140 <3263540140@qq.com>
2020-05-27 16:24:20 +0800

    插件升级为1.0.0版本。

--- FairyGUI_GenCode_Lua/main.lua
+++ FairyGUI_GenCode_Lua/main.lua
@@ -113,7 +113,7 @@ local function get_project_custom_property_value(key_name)
 end
 
 function onPublish(handler)
-    if not handler.genCode then
+    if not handler.genCode or handler.publishDescOnly then
         return
     end
 
@@ -131,10 +131,11 @@ end
 function genCode(handler)
     local settings = handler.project:GetSettings("Publish").codeGeneration
     local codePkgName = handler:ToFilename(handler.pkg.name); --convert chinese to pinyin, remove special chars etc.
-    fprint("codePkgName = " .. codePkgName);
     local exportCodePath = handler.exportCodePath .. '/' .. codePkgName
-    fprint("exportCodePath = " .. exportCodePath);
     local lua_file_extension_name = get_project_custom_property_value(customPropKeys.key_lua_file_extension_name.name);
+    local lua_path_root = get_project_custom_property_value(customPropKeys.key_lua_path_root.name);
+    lua_path_root = string.gsub(lua_path_root, "/", ".");
+    lua_path_root = string.gsub(lua_path_root, "\\", ".");
 
     local namespaceName = codePkgName
     local key_wrapper_namespace = get_project_custom_property_value(customPropKeys.key_wrapper_namespace.name);
@@ -148,7 +149,11 @@ function genCode(handler)
     handler:SetupCodeFolder(exportCodePath, lua_file_extension_name) --check if target folder exists, and delete old files
 
     local getMemberByName = settings.getMemberByName
-    local classTemplateTxt = CS.System.IO.File.ReadAllText(PluginPath .. "/class_template.txt");
+    local classTemplateTxt = CS.System.IO.File.ReadAllText(PluginPath .. "/component_template.txt");
+    if (classTemplateTxt == "") then
+        fprint("component_template.txt content null.")
+        return ;
+    end
 
     local classCnt = classes.Count
     local writer = LuaCodeWriter.new({ blockFromNewLine = false, usingTabs = true })
@@ -156,17 +161,7 @@ function genCode(handler)
         local _classTemplateTxt = classTemplateTxt;
         local classInfo = classes[i]
         local members = classInfo.members
-        local references = classInfo.references
         writer:reset()
-
-        local refCount = references.Count
-        if refCount > 0 then
-            for j = 0, refCount - 1 do
-                local ref = references[j]
-                writer:writeln('local %s = require("%s/%s");', ref, exportCodePath, ref)
-            end
-            writer:writeln()
-        end
 
         _classTemplateTxt = string.gsub(_classTemplateTxt, "$className", classInfo.className);
         _classTemplateTxt = string.gsub(_classTemplateTxt, "$superClassName", classInfo.superClassName);
@@ -175,16 +170,18 @@ function genCode(handler)
         else
             _classTemplateTxt = string.gsub(_classTemplateTxt, "$namespace", "");
         end
-        local _classMemberAnnotation = "";
+
+        local _classFieldAnnotation = string.format('\n---@field public %s %s', "__ui", classInfo.superClassName);
         local memberCnt = members.Count
         for j = 0, memberCnt - 1 do
             if (j > 0) then
-                _classMemberAnnotation = _classMemberAnnotation .. "\n";
+                _classFieldAnnotation = _classFieldAnnotation .. "\n";
             end
             local memberInfo = members[j]
-            _classMemberAnnotation = _classMemberAnnotation .. string.format('---@field public %s %s', memberInfo.varName, memberInfo.type);
+            _classFieldAnnotation = _classFieldAnnotation .. string.format('---@field public %s %s', memberInfo.varName, memberInfo.type);
         end
-        _classTemplateTxt = string.gsub(_classTemplateTxt, "$classMemberAnnotation", _classMemberAnnotation);
+
+        _classTemplateTxt = string.gsub(_classTemplateTxt, "$classFieldAnnotation", _classFieldAnnotation);
 
         local _urlValue = string.format('"ui://%s%s"', handler.pkg.id, classInfo.classId)
         _classTemplateTxt = string.gsub(_classTemplateTxt, "$urlValue", _urlValue);
@@ -192,34 +189,34 @@ function genCode(handler)
         _classTemplateTxt = string.gsub(_classTemplateTxt, "$uiPackageName", handler.pkg.name);
         _classTemplateTxt = string.gsub(_classTemplateTxt, "$uiResName", classInfo.resName);
 
-        local _classMemberInstatiation = "";
+        local _classFieldInstatiation = "";
         for j = 0, memberCnt - 1 do
             local memberInfo = members[j]
             if j > 0 then
-                _classMemberInstatiation = _classMemberInstatiation .. "\n";
+                _classFieldInstatiation = _classFieldInstatiation .. "\n";
             end
-            _classMemberInstatiation = _classMemberInstatiation .. "\t";
+            _classFieldInstatiation = _classFieldInstatiation .. "\t";
             if memberInfo.group == 0 then
                 if getMemberByName then
-                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetChild("%s");', memberInfo.varName, memberInfo.name);
+                    _classFieldInstatiation = _classFieldInstatiation .. string.format('self.%s = self.__ui:GetChild("%s");', memberInfo.varName, memberInfo.name);
                 else
-                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetChildAt("%s");', memberInfo.varName, memberInfo.index);
+                    _classFieldInstatiation = _classFieldInstatiation .. string.format('self.%s = self.__ui:GetChildAt("%s");', memberInfo.varName, memberInfo.index);
                 end
             elseif memberInfo.group == 1 then
                 if getMemberByName then
-                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetController("%s");', memberInfo.varName, memberInfo.name);
+                    _classFieldInstatiation = _classFieldInstatiation .. string.format('self.%s = self.__ui:GetController("%s");', memberInfo.varName, memberInfo.name);
                 else
-                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetControllerAt("%s");', memberInfo.varName, memberInfo.index);
+                    _classFieldInstatiation = _classFieldInstatiation .. string.format('self.%s = self.__ui:GetControllerAt("%s");', memberInfo.varName, memberInfo.index);
                 end
             else
                 if getMemberByName then
-                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetTransition("%s");', memberInfo.varName, memberInfo.name);
+                    _classFieldInstatiation = _classFieldInstatiation .. string.format('self.%s = self.__ui:GetTransition("%s");', memberInfo.varName, memberInfo.name);
                 else
-                    _classMemberInstatiation = _classMemberInstatiation .. string.format('self.%s = self:GetTransitionAt("%s");', memberInfo.varName, memberInfo.index);
+                    _classFieldInstatiation = _classFieldInstatiation .. string.format('self.%s = self.__ui:GetTransitionAt("%s");', memberInfo.varName, memberInfo.index);
                 end
             end
         end
-        _classTemplateTxt = string.gsub(_classTemplateTxt, "$classMemberInstantiation", _classMemberInstatiation);
+        _classTemplateTxt = string.gsub(_classTemplateTxt, "$classFieldInstantiation", _classFieldInstatiation);
         writer:writeln('%s', _classTemplateTxt);
 
         writer:save(exportCodePath .. '/' .. classInfo.className .. '.' .. lua_file_extension_name)
@@ -228,19 +225,20 @@ function genCode(handler)
     writer:reset()
 
     local binderTemplateTxt = CS.System.IO.File.ReadAllText(PluginPath .. "/binder_template.txt");
-
+    if (binderTemplateTxt == "") then
+        fprint("binder_template.txt content null.")
+        return ;
+    end
     local binderName = codePkgName .. 'Binder'
 
     local _requireStatement = "";
-    local lua_path_root = get_project_custom_property_value(customPropKeys.key_lua_path_root.name);
-    lua_path_root = string.gsub(lua_path_root, "/", ".");
-    lua_path_root = string.gsub(lua_path_root, "\\", ".");
+
     for i = 0, classCnt - 1 do
         if (i > 0) then
             _requireStatement = _requireStatement .. "\n";
         end
         local classInfo = classes[i]
-        _requireStatement = _requireStatement .. string.format('local %s = require("%s%s.%s");', classInfo.className, lua_path_root,codePkgName, classInfo.className);
+        _requireStatement = _requireStatement .. string.format('%s = require("%s%s.%s");', classInfo.className, lua_path_root, codePkgName, classInfo.className);
     end
 
     binderTemplateTxt = string.gsub(binderTemplateTxt, "$requireStatement", _requireStatement);
@@ -255,18 +253,32 @@ function genCode(handler)
         _bindStatement = _bindStatement .. "\t";
 
         if (key_wrapper_namespace ~= "") then
-            _bindStatement = _bindStatement .. string.format('%sUIObjectFactory.SetExtension(%s.URL, %s);', key_wrapper_namespace .. ".", classInfo.className, classInfo.className);
+            _bindStatement = _bindStatement .. string.format('%sUIObjectFactory.SetPackageItemExtension(%s.URL, typeof(%s));', key_wrapper_namespace .. ".", classInfo.className, classInfo.className);
         else
-            _bindStatement = _bindStatement .. string.format('UIObjectFactory.SetExtension(%s.URL, %s);', classInfo.className, classInfo.className);
+            _bindStatement = _bindStatement .. string.format('UIObjectFactory.SetPackageItemExtension(%s.URL,typeof(%s));', classInfo.className, classInfo.className);
         end
 
 
     end
     binderTemplateTxt = string.gsub(binderTemplateTxt, "$bindStatement", _bindStatement);
     writer:writeln('%s', binderTemplateTxt);
-
     writer:save(exportCodePath .. '/' .. binderName .. '.' .. lua_file_extension_name)
 
+    writer:reset()
+
+    local _readSuccess, _initTxt = pcall(function()
+        return CS.System.IO.File.ReadAllText(handler.exportCodePath .. "/init.lua");
+    end);
+    if(not _readSuccess) then
+        _initTxt = "";
+    end
+    local _requireStatement = string.format('local %s = require("%s%s.%s");',binderName, lua_path_root,codePkgName, binderName);
+    _requireStatement = _requireStatement .. string.format('\n%s:BindAll();',binderName);
+    if(string.find(_initTxt,_requireStatement,1,true) == nil) then
+        _requireStatement = _requireStatement .. "\n";
+        _initTxt = _initTxt .. _requireStatement;
+        CS.System.IO.File.WriteAllText(handler.exportCodePath .. "/init.lua", _initTxt);
+    end
 end
 
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # FairyGUI_GenCode_Lua
 
 #### 介绍
-FairyGUI.v2020 编辑器插件，用于生成 Lua 视图绑定代码。
+FairyGUI.v2020 编辑器插件，用于生成 Lua UI绑定代码。
 
 #### 安装教程
 
@@ -9,9 +9,9 @@ FairyGUI.v2020 编辑器插件，用于生成 Lua 视图绑定代码。
 
 #### 使用说明
 
-1.  本项目理论上适用使用 Lua 作为视图逻辑的游戏引擎，但只在 Unity 引擎下进行了测试。
+1.  本项目理论上适用使用 Lua 作为UI逻辑的游戏引擎，但只在 Unity 引擎下进行了测试。
 2.  使用本插件前，需要先熟悉编辑器发布代码的功能，并开启发布代码。
-3.  使用__项目设置——自定义属性__为插件功能赋值。
+3.  使用 编辑器—项目设置—自定义属性 为插件功能赋值。
 ```
 local customPropKeys = {
     key_gen_lua = { name = "key_gen_lua", default_value = "true" },
@@ -23,7 +23,20 @@ local customPropKeys = {
 * key_gen_lua 是否生成Lua代码，默认为值 true
 * key_lua_file_extension_name Lua文件的扩展名，默认值为 lua
 * key_lua_path_root 生成文件相对于Lua执行根路径的相对路径，默认值为 UIGenCode/
-* key_wrapper_namespace 引擎层 FairyGUI 导出代码的命名空间，默认值为 CS.FairyGUI
+* key_wrapper_namespace 框架层 FairyGUI 导出代码的命名空间，默认值为 CS.FairyGUI
 
-4.  生成文件是根据模板文件 __class_template.txt__ 和 __binder_template.txt__ 生成,以 __$XXX__ 作为占位符,可以在此基础上继续自定义扩展。
-5.  本项目参考了官方的 [GenCode](https://github.com/fairygui/FairyGUI-Editor) 插件，作为补充，提供Lua生成作为一个选项。
+4.  生成文件是根据模板文件 __component_template.txt__ 和 __binder_template.txt__ 生成，以 __$XXX__ 作为占位符，可以在此基础上继续自定义扩展。
+5. 	使用生成文件示例:
+```
+	require("UIGenCode.init");
+	UI_DemoComponent:CreateInstance();
+    GRoot.inst:AddChild(UI_DemoComponent.__ui);
+	 UI_DemoComponent.m_btn_login.onClick:Add(function()
+        print("btn clicked");
+    end);
+```
+* 绑定类是自动调用绑定方法的，只需要引入一次 init.lua 文件即可。
+* 为了使用方便，生成的组件类都是全局名称，这一点需要注意。如果不需要生成全局名称，则修改插件源码。
+* 组件实例默认赋值为 __ui 对象，为了防止被组件子级覆盖，使用了双下划线命名。
+* 不要手动修改生成文件，以免被下一次生成覆盖，更好的办法是修改插件源码和模板文件。
+6.  本项目参考了官方的 [GenCode](https://github.com/fairygui/FairyGUI-Editor) 插件，作为补充，提供Lua生成作为一个选项。
